77f3e011888c24a745c91d61a6e9af02ad728d39
sergio mendieta <sermendiarias@gmail.com>
2019-10-10 23:32:59 -0500

    [oigres] Setting up the project

---
+++ src/main/java/com/sonartype/numbertowords/NumbersToEnglishWordsApplication.java
@@ -0,0 +1,13 @@
+package com.sonartype.numbertowords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbersToEnglishWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbersToEnglishWordsApplication.class, args);
+	}
+
+}
